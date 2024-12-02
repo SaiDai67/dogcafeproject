fd059011ee10ce932704a57f915c67da5e1d68f0
bln51000 <shanegibson1221@gmail.com>
2024-12-02 16:13:01 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,16 @@ function Home() {
     <div style={{ textAlign: "center" }}>
       <h1>Welcome to Dog Café 🐾</h1>
       <p>Enjoy coffee with your furry friends!</p>
+      <p>Set your appointment today!</p>
+      <h>Hours</h>
+      <p>Monday: 7am-8pm
+      Tuesday: 7am-8pm
+      Wednesday: 7am-8pm
+      Thursday: 7am-8pm
+      Friday: 7am-9pm
+      Saturday: 10am-10pm
+      Sunday: 10am-6pm
+      </p>
     </div>
   );
 }
